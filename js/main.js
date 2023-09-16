639d5af37921e1d3da704cfaaae15ebb585a9409
sardor86 <shavkatovs721@gmail.com>
2023-09-16 18:06:27 +0500

    added header

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function onmouseover_header(id_element)
+{
+    const element = document.getElementById(id_element);
+    element.style.display = "block"
+}
+
+function onmouseout_header(id_element)
+{
+    const element = document.getElementById(id_element);
+    element.style.display = "none"
+}
